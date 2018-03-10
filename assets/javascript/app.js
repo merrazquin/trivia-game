52eb10602d5eebcfe34592ac726c6f9760d7c7be
Marcela Errazquin <merrazquin@gmail.com>
2018-03-10 17:52:13 -0500

    [formatting] Added folding regions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,8 @@
 $(function () {
-    //constants
+    //#region constants
     const QUESTION_TIME = 5;
     const REMEDIATION_TIME = 2000;
+    //#endregion constants
    
     // data
     var triviaData = [
@@ -27,7 +28,7 @@ $(function () {
         }
     ];
 
-    // UI elements
+    //#region UI elements
     var intro = $("#intro");
     var startBtn = $("#start");
     var restartBtn = $("#restart")
@@ -42,25 +43,34 @@ $(function () {
     var correctDisplay = $("#correct");
     var incorrectDisplay = $("#incorrect");
     var timedOutDisplay = $("#timed-out");
-
-    timerDisplay.pietimer({seconds: QUESTION_TIME, color: 'rgba(0, 0, 0, 0.8)', height:40, width:40}, timedOut);
+    //#endregion UI elements
  
-    // variables
+    //#region variables
     var displays = [introDisplay, questionDisplay, remediationDisplay, outro];
     var questionsCorrect, questionsIncorrect, questionsUnanswered, questionIndex;
     var currentQuestion;
     var timerID;
+    //#endregion variables
 
+    // create a pie timer to show question time countdown
+    timerDisplay.pietimer({seconds: QUESTION_TIME, color: 'rgba(0, 0, 0, 0.8)', height:40, width:40}, timedOut);
+
+    //#region game functions
     function resetGame() {
         questionsCorrect = questionsIncorrect = questionsUnanswered = questionIndex = 0;
         changeState(introDisplay, false);
     }
-
-    function changeState(activeDisplay, showTimer) {
-        showTimer ? timerDisplay.show() : timerDisplay.hide();
-        displays.forEach(display => {
-            display === activeDisplay ? display.show() : display.hide();
-        });
+    
+    function nextQuestion() {
+        if(questionIndex == triviaData.length) {
+            endGame();
+            return;
+        }
+        changeState(questionDisplay, true);
+        displayQuestion(triviaData[questionIndex]);
+        questionIndex++;
+        timerDisplay.pietimer('reset');
+        timerDisplay.pietimer('start');
     }
 
     function displayQuestion(questionObj) {
@@ -85,28 +95,16 @@ $(function () {
         remediate();
     }
 
-    function remediate() {
-        changeState(remediationDisplay, true);
-        setTimer(REMEDIATION_TIME, nextQuestion);
-    }
-
-    function nextQuestion() {
-        if(questionIndex == triviaData.length) {
-            endGame();
-            return;
-        }
-        changeState(questionDisplay, true);
-        displayQuestion(triviaData[questionIndex]);
-        questionIndex++;
-        timerDisplay.pietimer('reset');
-        timerDisplay.pietimer('start');
-    }
-
     function timedOut() {
         questionsUnanswered++;
 
         feedback.text("Timed out");
         remediate();
+    }
+
+    function remediate() {
+        changeState(remediationDisplay, true);
+        setTimer(REMEDIATION_TIME, nextQuestion);
     }
 
     function endGame() {
@@ -115,19 +113,28 @@ $(function () {
         timedOutDisplay.text(questionsUnanswered);
         changeState(outro, false);
     }
+    //#endregion game functions
 
-    // listeners
+    //#region listeners
     startBtn.click(nextQuestion);
-
-    $("button.answer").click(handleAnswer);
     restartBtn.click(() => {resetGame(); nextQuestion();});
 
-    // helper functions
+    $("button.answer").click(handleAnswer);
+    //#endregion listeners
+
+    //#region helper functions
     function setTimer(time, callback) {
         clearTimeout(timerID);
         timerID = setTimeout(callback, time)
     }
-
+    
+    function changeState(activeDisplay, showTimer) {
+        showTimer ? timerDisplay.show() : timerDisplay.hide();
+        displays.forEach(display => {
+            display === activeDisplay ? display.show() : display.hide();
+        });
+    }
+    //#endregion helper functions
 
     // calls
     resetGame();
